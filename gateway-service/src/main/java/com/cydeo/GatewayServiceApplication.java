729322c94044c3bcae40bb787b80c21df692b53f
musafir928 <uganoghli@gmail.com>
2022-07-30 23:20:03 +0200

    prepare for GatewayServiceApplication

---
+++ gateway-service/src/main/java/com/cydeo/GatewayServiceApplication.java
@@ -0,0 +1,9 @@
+package com.cydeo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// spring annotations would not work till add a dependency to the child pom from parent pom
+public class GatewayServiceApplication {
+
+}
